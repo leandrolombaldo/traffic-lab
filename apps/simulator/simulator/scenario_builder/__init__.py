5ef43d84228407e6b85308f0345ec5632c467cb6
Leandro lombaldo <leandrosilva.hair@gmail.com>
2026-05-24 21:12:46 -0300

    feat: add scenario builder package

---
+++ apps/simulator/simulator/scenario_builder/__init__.py
@@ -0,0 +1,13 @@
+from simulator.scenario_builder.models import (
+    InterventionSpec,
+    InterventionType,
+    ProjectSpec,
+    ScenarioSpec,
+)
+
+__all__ = [
+    "InterventionSpec",
+    "InterventionType",
+    "ProjectSpec",
+    "ScenarioSpec",
+]
